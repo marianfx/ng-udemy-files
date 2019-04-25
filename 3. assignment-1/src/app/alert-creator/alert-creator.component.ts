9407de604fca0435a4b069fbf85aea2944e2d2f0
Marian Focsa <marian.focsa@kireygroup.com>
2019-04-25 16:15:29 +0300

    Added files for first assignment

---
+++ 3. assignment-1/src/app/alert-creator/alert-creator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alert-creator',
+  templateUrl: './alert-creator.component.html',
+  styleUrls: ['./alert-creator.component.css']
+})
+export class AlertCreatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
