cdc5f222ec0afdf192a2b638636e9d93021b8601
Marian Focsa <marian.focsa@kireygroup.com>
2019-05-30 17:33:01 +0300

    Added example on how Style Encapsulation works

--- 7. components-databinding/src/app/server-element/server-element.component.ts
+++ 7. components-databinding/src/app/server-element/server-element.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  // encapsulation: ViewEncapsulation.Emulated // the default behavior; the styles applied only for this component
+  // encapsulation: ViewEncapsulation.Native // same as emulated, but with natural browser ShadowDom usage (only for compatible browsers)
+  // encapsulation: ViewEncapsulation.None // no encapsulation => styles applied globally
 })
 export class ServerElementComponent implements OnInit {
   // data binding from parent to child
