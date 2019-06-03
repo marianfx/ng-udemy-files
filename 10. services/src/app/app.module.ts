b38da9f92fc80853001c97f70d9d662b48252666
Marian Focsa <marian.focsa@kireygroup.com>
2019-06-03 17:52:22 +0300

    Added logging service (nw)

--- 10. services/src/app/app.module.ts
+++ 10. services/src/app/app.module.ts
@@ -1,21 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
-    AccountComponent,
-    NewAccountComponent
+    NewAccountComponent,
+    AccountComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
