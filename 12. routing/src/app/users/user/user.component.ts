4db18c396feee5480e435473e86662f4bd3c986e
Marian Focsa <marian.focsa@kireygroup.com>
2019-06-04 17:06:05 +0300

    Added URL parameters and how they work with subscription and shapshot

--- 12. routing/src/app/users/user/user.component.ts
+++ 12. routing/src/app/users/user/user.component.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
+  paramsSubscription: Subscription;
   user: {id: number, name: string};
 
-  constructor() { }
+  constructor(private activeRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    // note: Snapshot is activated only at the first appeareance of the component (first init)
+    let id = this.activeRoute.snapshot.params["id"];
+    let name = this.activeRoute.snapshot.params["name"];
+    this.user = { id: id, name: name};
+
+    // the proper way => subscribe to params change (will execute at each change)
+    this.paramsSubscription = this.activeRoute.params.subscribe((newParams: Params) => {
+        this.user.id = newParams.id;
+        this.user.name = newParams.name;
+    });
   }
 
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe(); // subscriptions remain in memory if not unsubscribed, because they are external
+  }
 }
