e7b01def49122711e7b167d30728ed817dbc7916
Marian Focsa <marian.focsa@kireygroup.com>
2019-06-05 15:30:19 +0300

    Added ROUTING to the main app part + recipe sub-module

---
+++ 6. the-project/src/app/app-routing.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from '@angular/router';
+import { RecipesComponent } from "./recipes/recipes.component";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
+import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
+import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/recipes',
+    pathMatch: "full" // '' is part of all paths; this makes it redirect only when on home
+  },
+  {
+    path: 'recipes',
+    component: RecipesComponent,
+    children: [
+      {
+        path: '',
+        component: RecipeStartComponent
+      },
+      {
+        path: 'new', // before :id because it doesn't know if 'new' is an ID
+        component: RecipeEditComponent
+      },
+      {
+        path: ':id',
+        component: RecipeDetailComponent
+      },
+      {
+        path: ':id/edit',
+        component: RecipeEditComponent
+      }
+    ]
+  },
+  {
+    path: 'shopping-list',
+    component: ShoppingListComponent
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
