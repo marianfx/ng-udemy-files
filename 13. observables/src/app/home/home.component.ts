b965ad05bbe4e6e103698450fb8adc20273f0271
Marian Focsa <marian.focsa@kireygroup.com>
2019-06-05 15:58:22 +0300

    Added a custom observer and events handling (data, error, completion)

--- 13. observables/src/app/home/home.component.ts
+++ 13. observables/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
-import { Subscription } from 'rxjs';
+import { Subscription, Observable, Observer } from 'rxjs';
 import { interval } from 'rxjs/observable/interval';
 
 @Component({
@@ -16,8 +16,34 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.firstObsSubscription = interval(1000).subscribe(count => {
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // });
+    const customIntervalObservable = Observable.create((observer: Observer<number>) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(++count);
+
+        // custom complete => STOP
+        if (count === 2) {
+          observer.complete();
+        }
+
+        // custom error
+        if (count > 3) {
+          observer.error(new Error("Count is bigger than 3"));
+        }
+      }, 1000);
+    });
+
+    // .subscribe(onData(data), onError(error), onCompleted());
+    this.firstObsSubscription = customIntervalObservable.subscribe(count => {
       console.log(count);
+    }, error => {
+      if (error)
+        console.log("Had some error, sorry", error);
+    }, () => {
+      console.log("Completed!"); // does not trigger on error
     });
   }
 
