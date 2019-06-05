7abbdb8206c6d98e54a0c24b7717d38c1b5544f1
Marian Focsa <marian.focsa@kireygroup.com>
2019-06-05 16:05:15 +0300

    Added pipe + map & filter operators examples

--- 13. observables/src/app/home/home.component.ts
+++ 13. observables/src/app/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { Subscription, Observable, Observer } from 'rxjs';
-import { interval } from 'rxjs/observable/interval';
+// import { interval } from 'rxjs/observable/interval';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -25,19 +26,29 @@ export class HomeComponent implements OnInit, OnDestroy {
         observer.next(++count);
 
         // custom complete => STOP
-        if (count === 2) {
-          observer.complete();
-        }
+        // if (count === 2) {
+        //   observer.complete();
+        // }
 
         // custom error
         if (count > 3) {
-          observer.error(new Error("Count is bigger than 3"));
+          observer.error(new Error("Count is bigger than the maximum"));
         }
       }, 1000);
     });
 
+    // PIPE + FILTER (filters out some results)
+    let filteredObservable = customIntervalObservable.pipe(filter((data: number) => {
+      return data > 1;
+    }));
+
+    // PIPE + MAP (maps result under another form)
+    let pipedObservable = filteredObservable.pipe(map((data: number) => {
+      return "Round: " + (data + 1);
+    }));
+
     // .subscribe(onData(data), onError(error), onCompleted());
-    this.firstObsSubscription = customIntervalObservable.subscribe(count => {
+    this.firstObsSubscription = pipedObservable.subscribe(count => {
       console.log(count);
     }, error => {
       if (error)
