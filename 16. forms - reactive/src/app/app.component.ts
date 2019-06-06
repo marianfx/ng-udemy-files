0ddc9db8e5e4df4b3c39ee14a30af96eb7ee2d53
Marian Focsa <marian.focsa@kireygroup.com>
2019-06-06 16:38:58 +0300

    Added custom validator and custom error code

--- 16. forms - reactive/src/app/app.component.ts
+++ 16. forms - reactive/src/app/app.component.ts
@@ -8,13 +8,14 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
+  forbiddenUsernames = ['Chris', 'Anna'];
   signupForm: FormGroup;
 
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
       "userData": new FormGroup({
-        "username": new FormControl(null, Validators.required),
+        "username": new FormControl(null, [Validators.required, this.forbiddenNamesValidator.bind(this)]),
         "email": new FormControl(null, [Validators.required, Validators.email])
       }),
       "gender": new FormControl("male"),
@@ -29,5 +30,13 @@ export class AppComponent implements OnInit {
   onAddHobby() {
     const newHobby = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get("hobbies")).push(newHobby);
+  }
+
+  forbiddenNamesValidator(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return { 'nameIsForbidden': true };
+    }
+
+    return null;
   }
 }
