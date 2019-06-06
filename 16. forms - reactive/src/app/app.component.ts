f06b08f1ccaee32068ae55fce4d2efe1b0e36447
Marian Focsa <marian.focsa@kireygroup.com>
2019-06-06 16:22:59 +0300

    Added Validators and Control access

--- 16. forms - reactive/src/app/app.component.ts
+++ 16. forms - reactive/src/app/app.component.ts
@@ -13,8 +13,8 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
-      "username": new FormControl(null),
-      "email": new FormControl(null),
+      "username": new FormControl(null, Validators.required),
+      "email": new FormControl(null, [Validators.required, Validators.email]),
       "gender": new FormControl("male")
     });
   }
