68d31e7e4180ff97150dcddac23595b311423b1d
Marian Focsa <marian.focsa@kireygroup.com>
2019-06-27 11:14:58 +0300

    Moved the routing to its own module (Recipes)

---
+++ 6. the-project/src/app/recipes/recipes-routing.module.ts
@@ -0,0 +1,45 @@
+import { Routes, RouterModule } from "@angular/router";
+import { RecipesComponent } from "./recipes.component";
+import { AuthGuard } from "../auth/auth.guard";
+import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipesResolverService } from "./recipes-resolver.service";
+import { NgModule } from "@angular/core";
+
+const appRoutes: Routes = [
+  {
+    path: 'recipes',
+    component: RecipesComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: '',
+        component: RecipeStartComponent
+      },
+      {
+        path: 'new', // before :id because it doesn't know if 'new' is an ID
+        component: RecipeEditComponent
+      },
+      {
+        path: ':id',
+        component: RecipeDetailComponent,
+        resolve: [RecipesResolverService]
+      },
+      {
+        path: ':id/edit',
+        component: RecipeEditComponent,
+        resolve: [RecipesResolverService]
+      }
+    ]
+  }
+];
+
+// in other modules, use forChild, because it's not at root of the app
+@NgModule({
+  imports: [RouterModule.forChild(appRoutes)],
+  exports: [RouterModule]
+})
+export class RecipesRoutingModule {
+
+}
--- 6. the-project/src/app/recipes/recipes.module.ts
+++ 6. the-project/src/app/recipes/recipes.module.ts
@@ -5,9 +5,9 @@ import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
 import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
 import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
 import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
-import { RouterModule } from "@angular/router";
 import { ReactiveFormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
+import { RecipesRoutingModule } from "./recipes-routing.module";
 
 @NgModule({
   declarations: [
@@ -19,17 +19,9 @@ import { CommonModule } from "@angular/common";
     RecipeEditComponent,
   ],
   imports: [
-    RouterModule,
+    RecipesRoutingModule,
     CommonModule, // don't import BrowserModule, as it does startup stuff
     ReactiveFormsModule,
-  ],
-  exports: [
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    RecipeStartComponent,
-    RecipeEditComponent,
   ]
 })
 export class RecipesModule {
