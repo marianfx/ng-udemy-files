d900ab52446b971215ccac0df3fd492f50f36668
Marian Focsa <marian.focsa@kireygroup.com>
2019-06-28 16:28:58 +0300

    Added example on how to trigger animations both ways and the shrink animation

--- 21. animations/src/app/app.component.ts
+++ 21. animations/src/app/app.component.ts
@@ -14,14 +14,31 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
         'background-color': 'blue',
         transform: 'translateX(100px)'
       })),
+      transition('normal <=> highlighted', animate(300))
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px)'
+      })),
+      state('shrunken', style({
+        'background-color': 'green',
+        transform: 'translateX(0px) scale(0.5)'
+      })),
       transition('normal => highlighted', animate(300)),
-      transition('highlighted => normal', animate(800))
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', animate(500))
     ])
   ]
 })
 export class AppComponent {
   list = ['Milk', 'Sugar', 'Bread'];
   state = 'normal';
+  wildState = 'normal';
 
   onAdd(item) {
     this.list.push(item);
@@ -29,5 +46,10 @@ export class AppComponent {
 
   onAnimate() {
     this.state = this.state === 'normal' ? 'highlighted' : 'normal';
+    this.wildState = this.wildState === 'normal' ? 'highlighted' : 'normal';
+  }
+
+  onShrink() {
+    this.wildState = this.wildState === 'shrunken' ? 'normal' : 'shrunken';
   }
 }
