61c5dc233abd6fa129a73ad34473ac61cfa654b2
Marian Focsa <marian.focsa@kireygroup.com>
2019-07-04 15:38:11 +0300

    Added Store-Devtools which allows easy debug from Chrome

--- 6. the-project/src/app/app.module.ts
+++ 6. the-project/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 
 
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { StoreModule } from '@ngrx/store';
 import * as fromApp from './store/app.reducer';
 import { EffectsModule } from '@ngrx/effects';
 import { AuthEffects } from './auth/store/auth.effects';
+import { environment } from '../environments/environment.prod';
 
 // remove stuff from 'imports', but also remove import at the top because otherwise it does not reduce code size
 
@@ -28,6 +30,7 @@ import { AuthEffects } from './auth/store/auth.effects';
     HttpClientModule,
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({ logOnly: environment.production }),
     AuthModule,
     ShoppingListModule,
     SharedThingsModule,
